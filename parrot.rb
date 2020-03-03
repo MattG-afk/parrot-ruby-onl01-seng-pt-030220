418760d8bb6a5aa131d5003679fd1b593ffe35a8
Matt Gordon <bigmattsize21@gmail.com>
2020-03-03 21:48:26 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(phrase="Pretty bird!")
-   puts "Squawk!{phrase}"
+   puts "Squawk!"
    
    "Squawk!"
+   
+   return {phrase}
    
 end
 parrot
